675ae76c1b7ada222248e53c26bc217d0f90852b
curoky <cccuroky@gmail.com>
2022-12-11 01:44:08 +0800

    feat(images): update nix/pip package list

--- dists/docker/script/setup-pkg-nix.sh
+++ dists/docker/script/setup-pkg-nix.sh
@@ -26,47 +26,56 @@ export PATH=/nix/var/nix/profiles/default/bin:$PATH
 # The following packages are not working properly with nix.
 # nixpkgs.cmake nixpkgs.conan nixpkgs.autoconf nixpkgs.bazel_5
 
-# nix-env -iA \
-nix-env -p /nix/var/nix/profiles/default -iA \
-  nixpkgs.automake nixpkgs.libtool nixpkgs.pkg-config nixpkgs.gnumake nixpkgs.ninja nixpkgs.distcc \
-  nixpkgs.ccache nixpkgs.scons nixpkgs.meson nixpkgs.ant nixpkgs.poetry nixpkgs.yarn nixpkgs.pipenv \
-  nixpkgs.gradle nixpkgs.maven nixpkgs.pre-commit nixpkgs.nodePackages.prettier nixpkgs.yapf \
-  nixpkgs.shfmt nixpkgs.buildifier nixpkgs.nodePackages.eslint nixpkgs.lua nixpkgs.nodejs \
-  nixpkgs.go_1_19 nixpkgs.ruby nixpkgs.rustup nixpkgs.jdk nixpkgs.thrift nixpkgs.vlang \
-  nixpkgs.git nixpkgs.lazygit nixpkgs.git-absorb nixpkgs.git-extras \
-  nixpkgs.git-lfs nixpkgs.bzip2 nixpkgs.xz nixpkgs.zstd nixpkgs.zip nixpkgs.unzip nixpkgs.iproute2 \
-  nixpkgs.iputils nixpkgs.netcat nixpkgs.lsof nixpkgs.htop nixpkgs.connect nixpkgs.procps \
-  nixpkgs.gost nixpkgs.nettools nixpkgs.zsh nixpkgs.starship nixpkgs.direnv nixpkgs.asciinema \
-  nixpkgs.tmux nixpkgs.tmuxinator nixpkgs.flex nixpkgs.bison nixpkgs.gettext nixpkgs.m4 \
-  nixpkgs.gnupatch nixpkgs.jq nixpkgs.vim nixpkgs.helix nixpkgs.wget nixpkgs.aria2 nixpkgs.rsync \
-  nixpkgs.gdb nixpkgs.ncdu nixpkgs.file nixpkgs.silver-searcher nixpkgs.gperf nixpkgs.watchman \
-  nixpkgs.nnn nixpkgs.ranger nixpkgs.exa nixpkgs.fzf nixpkgs.bat nixpkgs.cloc nixpkgs.atuin \
-  nixpkgs.go-task nixpkgs.krb5 nixpkgs.less nixpkgs.navi nixpkgs.ghq nixpkgs.lcov nixpkgs.gcovr \
-  nixpkgs.opencc nixpkgs.dolt nixpkgs.include-what-you-use nixpkgs.mold nixpkgs.parallel \
-  nixpkgs.fd nixpkgs.wishlist nixpkgs.sqlcipher nixpkgs.sqlite nixpkgs.zoxide nixpkgs.mkdocs \
-  nixpkgs.sphinx nixpkgs.hugo nixpkgs.protobuf nixpkgs.buck nixpkgs.clang-tools_14 \
-  nixpkgs.cmake-format nixpkgs.shellcheck nixpkgs.pandoc nixpkgs.dstat nixpkgs.rustup \
-  nixpkgs.flamegraph nixpkgs.nixpkgs-fmt nixpkgs.ansible nixpkgs.earthly \
-  nixpkgs.cppcheck nixpkgs.graphviz nixpkgs.lld_14 nixpkgs.gdu nixpkgs.git-filter-repo \
-  nixpkgs.jemalloc nixpkgs.ethtool nixpkgs.tcpdump
+pkg_list=(
+  # build tools
+  nixpkgs.automake nixpkgs.libtool nixpkgs.pkg-config nixpkgs.gnumake nixpkgs.ninja nixpkgs.distcc
+  nixpkgs.ccache nixpkgs.poetry nixpkgs.yarn nixpkgs.pipenv nixpkgs.gradle nixpkgs.maven
+  # nixpkgs.buck nixpkgs.scons nixpkgs.meson nixpkgs.ant
+  nixpkgs.include-what-you-use nixpkgs.mold nixpkgs.clang-tools_14 nixpkgs.lld_14
+  nixpkgs.nodejs nixpkgs.go_1_19 nixpkgs.ruby # nixpkgs.lua nixpkgs.vlang nixpkgs.rustup
+  # formatter and linter
+  nixpkgs.pre-commit nixpkgs.nodePackages.prettier nixpkgs.yapf nixpkgs.nixpkgs-fmt
+  nixpkgs.shfmt nixpkgs.buildifier nixpkgs.nodePackages.eslint nixpkgs.cppcheck
+  nixpkgs.cmake-format nixpkgs.shellcheck
+  # git
+  nixpkgs.git nixpkgs.lazygit nixpkgs.git-absorb nixpkgs.git-extras nixpkgs.git-lfs
+  nixpkgs.git-filter-repo
+  # compress
+  nixpkgs.bzip2 nixpkgs.xz nixpkgs.zstd nixpkgs.zip nixpkgs.unzip
+  # sre
+  nixpkgs.iproute2 nixpkgs.iputils nixpkgs.netcat nixpkgs.lsof nixpkgs.htop nixpkgs.connect
+  nixpkgs.procps nixpkgs.gost nixpkgs.nettools nixpkgs.fd nixpkgs.ethtool nixpkgs.tcpdump
+  nixpkgs.dstat nixpkgs.wget nixpkgs.aria2 nixpkgs.rsync nixpkgs.file nixpkgs.gperf
+  nixpkgs.gdb
+  # doc
+  nixpkgs.mkdocs nixpkgs.sphinx nixpkgs.hugo nixpkgs.pandoc
+  # shell
+  nixpkgs.zsh nixpkgs.starship nixpkgs.direnv nixpkgs.tmux nixpkgs.tmuxinator
+  # editor
+  nixpkgs.flex nixpkgs.bison nixpkgs.helix nixpkgs.gnupatch nixpkgs.gettext nixpkgs.m4
+  nixpkgs.jq nixpkgs.vim nixpkgs.bat nixpkgs.less
+  # tools
+  nixpkgs.graphviz nixpkgs.asciinema nixpkgs.gdu nixpkgs.jemalloc nixpkgs.ncdu
+  nixpkgs.silver-searcher nixpkgs.watchman nixpkgs.opencc nixpkgs.dolt nixpkgs.parallel
+  nixpkgs.exa nixpkgs.fzf nixpkgs.cloc                    # nixpkgs.atuin nixpkgs.nnn nixpkgs.ranger
+  nixpkgs.go-task nixpkgs.krb5 nixpkgs.lcov nixpkgs.gcovr # nixpkgs.navi nixpkgs.ghq
+  nixpkgs.wishlist nixpkgs.sqlcipher nixpkgs.sqlite nixpkgs.zoxide
+  nixpkgs.flamegraph nixpkgs.ansible nixpkgs.earthly
+)
+nix-env -p /nix/var/nix/profiles/default -iA "${pkg_list[@]}"
 
 # nix-env -iA -p /nix/var/nix/profiles/gcc49 nixpkgs.gcc49
-# nix-env -iA -p /nix/var/nix/profiles/gcc8 nixpkgs.gcc8
-# nix-env -iA -p /nix/var/nix/profiles/gcc11 nixpkgs.gcc11
 # nix-env -iA -p /nix/var/nix/profiles/gcc12 nixpkgs.gcc12
 # nix-env -iA -p /nix/var/nix/profiles/flutter nixpkgs.flutter
 # nix-env -iA -p /nix/var/nix/profiles/dart nixpkgs.dart
 nix-env -iA -p /nix/var/nix/profiles/protobuf3_17 nixpkgs.protobuf3_17
 nix-env -iA -p /nix/var/nix/profiles/jdk8 nixpkgs.jdk8
 nix-env -iA -p /nix/var/nix/profiles/jdk11 nixpkgs.jdk11
-nix-env -iA -p /nix/var/nix/profiles/jdk17 nixpkgs.jdk
+nix-env -iA -p /nix/var/nix/profiles/jdk19 nixpkgs.jdk19
 # nix-env -iA -p /nix/var/nix/profiles/dotnet nixpkgs.dotnet-sdk
 # nix-env -iA -p /nix/var/nix/profiles/perl nixpkgs.perl
 # nix-env -iA -p /nix/var/nix/profiles/python2 nixpkgs.python
 # nix-env -iA -p /nix/var/nix/profiles/python3 nixpkgs.python3
-# nix-env -iA -p /nix/var/nix/profiles/clang-tools_10 nixpkgs.clang-tools_10
-# nix-env -iA -p /nix/var/nix/profiles/clang-tools_11 nixpkgs.clang-tools_11
-# nix-env -iA -p /nix/var/nix/profiles/clang-tools_12 nixpkgs.clang-tools_12
 # nix-env -iA -p /nix/var/nix/profiles/clang-tools_13 nixpkgs.clang-tools_13
 nix-env -iA -p /nix/var/nix/profiles/llvm14 nixpkgs.clang_14
 nix-env -iA -p /nix/var/nix/profiles/llvm14 nixpkgs.clang-tools_14
@@ -74,9 +83,5 @@ nix-env -iA -p /nix/var/nix/profiles/llvm14 nixpkgs.llvmPackages_14.llvm
 nix-env -iA -p /nix/var/nix/profiles/llvm14-bintools nixpkgs.llvmPackages_14.bintools-unwrapped
 nix-env -iA -p /nix/var/nix/profiles/inetutils nixpkgs.inetutils
 nix-env -iA -p /nix/var/nix/profiles/coreutils nixpkgs.coreutils
-nix-env -iA -p /nix/var/nix/profiles/libs nixpkgs.boost
-# nix-env -iA -p /nix/var/nix/profiles/hadoop nixpkgs.hadoop
-# nix-env -iA -p /nix/var/nix/profiles/spark nixpkgs.spark
-# nix-env -iA -p /nix/var/nix/profiles/flink nixpkgs.flink
 
 nix-collect-garbage
