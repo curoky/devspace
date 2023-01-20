4f5e5124277ec652334f080e9f4dafc85335fe81
curoky <cccuroky@gmail.com>
2023-01-20 16:00:59 +0800

    feat(formula/exa-wrapper): remove patch"

--- third-party/homebrew/Formula/exa-wrapper.rb
+++ third-party/homebrew/Formula/exa-wrapper.rb
@@ -5,8 +5,6 @@ class ExaWrapper < Formula
   version 'head'
 
   def install
-    # TODO: remove this line. https://gist.github.com/eggbean/74db77c4f6404dd1f975bd6f048b86f8?permalink_comment_id=4442136#gistcomment-4442136
-    inreplace 'exa-wrapper.sh', 'meb == 0', 'meb == 1'
     bin.install 'exa-wrapper.sh' => 'ls'
   end
 
