71ca86749631478cc6def8ceb91f907882b02b21
curoky <cccuroky@gmail.com>
2023-03-08 19:30:36 +0800

    feat(config): remove local ips

--- config/linux/profile.d/homebrew-mirror-cn.sh
+++ config/linux/profile.d/homebrew-mirror-cn.sh
@@ -1,3 +1,20 @@
+# Copyright (c) 2018-2023 curoky(cccuroky@gmail.com).
+#
+# This file is part of dotbox.
+# See https://github.com/curoky/dotbox for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # https://mirrors.tuna.tsinghua.edu.cn/help/homebrew/
 
 # export HOMEBREW_INSTALL_FROM_API=1
