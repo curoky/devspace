7f4f014e3b18d2da52808375269ccddee1c8eedd
curoky <cccuroky@gmail.com>
2023-03-10 13:43:23 +0800

    feat(tool): add crontab runner

---
+++ tools/crontab-runner.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Copyright (c) 2018-2023 curoky(cccuroky@gmail.com).
+#
+# This file is part of dotbox.
+# See https://github.com/curoky/dotbox for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+mkdir -p ~/.cache/crontab
+logfile=~/.cache/crontab/"${1:-default}".log
+exec >>$logfile 2>&1
+
+echo "==================== start at ($(date '+%Y-%m-%d %H:%M:%S')) ======================="
+
+$2 "${@:3}"
+
+echo "==================== start end ($(date '+%Y-%m-%d %H:%M:%S'))======================"
