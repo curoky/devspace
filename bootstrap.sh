5210be8f37808635615eb0ad3ce286551e5ac9e4
curoky <cccuroky@gmail.com>
2023-05-02 22:00:53 +0800

    feat(config): update rime vscode brew dotdrop

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,12 +19,6 @@ set -xeuo pipefail
 
 cd "$(dirname $0)"
 
-export PATH=/usr/local/opt/openssl@3/bin:/Users/curoky/Library/Python/3.8/bin:$PATH
-
-if ! command -v dotdrop &>/dev/null; then
-  python3 -m pip install git+https://github.com/deadc0de6/dotdrop
-fi
-
 if [[ $(uname -s) == "Darwin" ]]; then
   dotdrop install --force --profile=macos-userconf
 else
