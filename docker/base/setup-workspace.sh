0489c990c06304ee318003a46e398fc0a1905370
curoky <cccuroky@gmail.com>
2024-07-29 02:14:07 +0800

    2024-07-29 02:14:06+08:00

---
+++ docker/base/setup-workspace.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# Copyright (c) 2018-2024 curoky(cccuroky@gmail.com).
+#
+# This file is part of dotbox.
+# See https://github.com/curoky/dotbox for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -xeuo pipefail
+
+cd /data/workspace
+
+git clone git@github.com:curoky/dotbox.git
+git clone git@github.com:curoky/minimal-example.git
+
+clone_all=${1:-0}
+if [ $clone_all -eq 1 ]; then
+  git clone git@github.com:curoky/learn-ml.git
+  git clone git@github.com:curoky/learn-cpp.git
+fi
