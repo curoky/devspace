4e61e02a7d2d2a6eb88f247bccbd1faccf229e49
curoky <cccuroky@gmail.com>
2024-10-16 19:37:06 +0800

    2024-10-16 19:37:06+08:00

--- third-party/nixpkgs/pkgs/nix/rime-bundle.nix
+++ third-party/nixpkgs/pkgs/nix/rime-bundle.nix
@@ -15,28 +15,28 @@ stdenv.mkDerivation rec {
   ];
 
   rime_emoji = fetchurl {
-    url = "https://github.com/rime/rime-emoji/archive/master.tar.gz";
-    sha256 = "sha256-HBccbmWCx/4xWf+qyImmxu4ybgM3iuGSIEFlUU0IdNU=";
+    url = "https://github.com/rime/rime-emoji/archive/be7d308e42c4c4485229de37ec0afb7bafbfafc0.tar.gz";
+    sha256 = "sha256-Bednn4sYYbqdyFEQGiCg/e2pOy1o2a3vdYyFSxgwDBI=";
   };
 
   meow_emoji_rime = fetchurl {
-    url = "https://github.com/hitigon/meow-emoji-rime/archive/master.tar.gz";
-    sha256 = "sha256-9MLmX/bV4M4DJ5Eha7ZgET8WMZAYtMU7pXsoD0gIiAo=";
+    url = "https://github.com/hitigon/meow-emoji-rime/archive/c2701e54b12649e690f35ac940ff6044ea1f0235.tar.gz";
+    sha256 = "sha256-DXIC6TvUgbQ5CvzI7fGx8Swb9gDzCugPyAptAo6cE9I=";
   };
 
   rime_prelude = fetchurl {
-    url = "https://github.com/rime/rime-prelude/archive/master.tar.gz";
-    sha256 = "sha256-RHx20UXSpvbYtuelcn6nkZOoyXmqzdZykg7AAojvks8=";
+    url = "https://github.com/rime/rime-prelude/archive/3803f09458072e03b9ed396692ce7e1d35c88c95.tar.gz";
+    sha256 = "sha256-r6B82YxUCgrC0dbpj/p3ZUnFNeYX0+Qetu+AgG34BvI=";
   };
 
   rime_symbols = fetchurl {
-    url = "https://github.com/fkxxyz/rime-symbols/archive/master.tar.gz";
-    sha256 = "sha256-+t3b3EoqFQ3Sl2UJBRQ+9h0sM26Y+o8QMxXKw+aDI5A=";
+    url = "https://github.com/fkxxyz/rime-symbols/archive/ce8fad81bc24c6c9fcb45b41ec062b94af9fcb46.tar.gz";
+    sha256 = "sha256-pjBXurzil0yJ490JaYUOTO8eKgzSUm1tkOdIDR/DI0U=";
   };
 
   rime_dict = fetchurl {
-    url = "https://github.com/Iorest/rime-dict/archive/master.tar.gz";
-    sha256 = "sha256-PFdXM6HtuxLrC/pikRzhfqhx0eotbLEQ2PA0+GkywjU=";
+    url = "https://github.com/Iorest/rime-dict/archive/325ecbda51cd93e07e2fe02e37e5f14d94a4a541.tar.gz";
+    sha256 = "sha256-2JmhFk1oAR/L4aHX6GUMOoIVo3baufRYoax7ogC59lQ=";
   };
 
   rime_cloverpinyin = fetchurl {
@@ -45,8 +45,8 @@ stdenv.mkDerivation rec {
   };
 
   rime_ice = fetchurl {
-    url = "https://github.com/iDvel/rime-ice/archive/main.tar.gz";
-    sha256 = "sha256-cH+7mHH4fuzuExJ94DGlJj9fIb8iEUUttrrk/MfMiU4=";
+    url = "https://github.com/iDvel/rime-ice/archive/87a7de47e92c124b7eda18fcd8e7fa3b9c42fa4e.tar.gz";
+    sha256 = "sha256-FBbXPxWT7ky+OmO6Lv+nEGx40GmcUQyvilbBnd6Rdwg=";
   };
 
   phases = ["buildPhase" "installPhase" ];
@@ -55,39 +55,37 @@ stdenv.mkDerivation rec {
   '';
 
   installPhase = ''
-    mkdir -p $out/share/rime-bundle/opencc/rime-emoji \
-      $out/share/rime-bundle/opencc/rime-symbols \
-      $out/share/rime-bundle/dicts/rime-dict \
-      $out/share/rime-bundle/dicts/rime-ice \
-      $out/share/rime-bundle/dicts/rime-cloverpinyin
+    mkdir -p \
+      $out/share/rime-bundle/rime-emoji \
+      $out/share/rime-bundle/rime-symbols \
+      $out/share/rime-bundle/rime-dict \
+      $out/share/rime-bundle/rime-ice \
+      $out/share/rime-bundle/rime-cloverpinyin
 
     # rime-symbols
-    #tar -xzf ${rime_symbols} --strip-components=1 -C $out/share/rime-bundle/opencc/rime-symbols
+    tar -xzf ${rime_symbols} --strip-components=1 -C $out/share/rime-bundle/rime-symbols
     #python3 $out/share/rime-bundle/opencc/rime-symbols/rime-symbols-gen
     #for file in $out/share/rime-bundle/opencc/rime-symbols/*.txt; do
     #  opencc -i $file -o "$out/share/rime-bundle/opencc/rime-symbols/simple.$(basename $file)" -c t2s.json
     #done
 
     # rime-emoji
-    mkdir ./rime-emoji
-    mkdir -p $out/share/rime-bundle/opencc/rime-emoji/opencc
-    tar -xzf ${rime_emoji} --strip-components=2 -C $out/share/rime-bundle/opencc/rime-emoji/
-    for file in $out/share/rime-bundle/opencc/rime-emoji/*.txt; do
-      opencc -i $file -o "$out/share/rime-bundle/opencc/rime-emoji/simple.$(basename $file)" -c t2s.json
+    tar -xzf ${rime_emoji} --strip-components=1 -C $out/share/rime-bundle/rime-emoji/
+    for file in $out/share/rime-bundle/rime-emoji/opencc/*.txt; do
+      opencc -i $file -o "$out/share/rime-bundle/rime-emoji/opencc/simple.$(basename $file)" -c t2s.json
     done
 
     # rime-dict
-    mkdir ./rime-dict
-    tar -xzf ${rime_dict} --strip-components=1 -C ./rime-dict
-    for file in ./rime-dict/**/*.dict.yaml; do
-      opencc -i $file -o "$out/share/rime-bundle/dicts/rime-dict/simple.$(basename $file)" -c t2s.json
+    tar -xzf ${rime_dict} --strip-components=1 -C $out/share/rime-bundle/rime-dict/
+    for file in $out/share/rime-bundle/rime-dict/**/*.dict.yaml; do
+      opencc -i $file -o "$out/share/rime-bundle/rime-dict/simple.$(basename $file)" -c t2s.json
     done
 
     # rime-cloverpinyin
-    unzip ${rime_cloverpinyin} -d $out/share/rime-bundle/dicts/rime-cloverpinyin
+    unzip ${rime_cloverpinyin} -d $out/share/rime-bundle/rime-cloverpinyin
 
     # rime-ice
-    tar -xzf ${rime_ice} --strip-components=1 -C $out/share/rime-bundle/dicts/rime-ice
+    tar -xzf ${rime_ice} --strip-components=1 -C $out/share/rime-bundle/rime-ice
   '';
 
   meta = with lib; {
