9e885e18f14faa60790733ed7fdb1260091ccaab
curoky <cccuroky@gmail.com>
2024-10-27 20:05:55 +0800

    2024-10-27 20:05:55+08:00

---
+++ host/linux/installer/install-docker-debian10.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+# Copyright (c) 2018-2024 curoky(cccuroky@gmail.com).
+#
+# This file is part of dotbox.
+# See https://github.com/curoky/dotbox for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -xeuo pipefail
+
+enable_gpu=${1:-0}
+
+tmp_dir=docker_pkgs
+mkdir -p $tmp_dir
+
+apt-get remove -y \
+  docker-ce docker-ce-cli containerd.io \
+  docker-buildx-plugin docker-compose-plugin \
+  nvidia-container-toolkit nvidia-container-toolkit-base \
+  libnvidia-container-tools libnvidia-container1 || echo ignore
+apt-get update -y
+apt-get install -y iptables
+
+curl -sSL -o $tmp_dir/libseccomp2_2.5.4-1+b3_amd64.deb \
+  http://ftp.debian.org/debian/pool/main/libs/libseccomp/libseccomp2_2.5.4-1+b3_amd64.deb
+dpkg -i $tmp_dir/libseccomp2_2.5.4-1+b3_amd64.deb
+
+# https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/
+
+pkg_list=(
+  containerd.io_1.6.22-1
+  docker-ce-cli_24.0.5-1~debian.11~bullseye
+  docker-ce_24.0.5-1~debian.11~bullseye
+  # docker-buildx-plugin_0.11.2-1~debian.11~bullseye
+  docker-buildx-plugin_0.16.2-1~debian.11~bullseye
+  docker-compose-plugin_2.20.2-1~debian.11~bullseye
+)
+
+for pkg in ${pkg_list[@]}; do
+  curl -sSL -o $tmp_dir/${pkg}.deb \
+    https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/${pkg}_amd64.deb
+  dpkg -i $tmp_dir/${pkg}.deb
+done
+
+if [[ $enable_gpu == 1 ]]; then
+  # https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
+  pkg_list=(
+    libnvidia-container1_1.13.5-1
+    libnvidia-container-tools_1.13.5-1
+    nvidia-container-toolkit-base_1.13.5-1
+    nvidia-container-toolkit_1.13.5-1
+  )
+  for pkg in ${pkg_list[@]}; do
+    curl -sSL -o $tmp_dir/${pkg}.deb \
+      https://github.com/NVIDIA/libnvidia-container/raw/gh-pages/stable/debian10/amd64/${pkg}_amd64.deb
+    dpkg -i $tmp_dir/${pkg}.deb
+  done
+fi
+
+# Post stage for docker
+
+# usermod -aG docker $USER
+
+# cat /etc/docker/daemon.json
+# {
+#     "data-root": "/data00/docker",
+#     "runtimes": {
+#         "nvidia": {
+#             "path": "nvidia-container-runtime",
+#             "runtimeArgs": []
+#         }
+#     }
+# }
+
+# docker run --rm --runtime=nvidia --gpus all ubuntu nvidia-smi
+
+systemctl daemon-reload
+systemctl restart docker
