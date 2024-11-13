198f3f1bb5a15ffe5a15a7fc7ab99ff114cbea8f
curoky <cccuroky@gmail.com>
2024-11-14 01:20:48 +0800

    2024-11-14 01:20:48+08:00

--- docker/stage/prebuilt/patch.sh
+++ docker/stage/prebuilt/patch.sh
@@ -44,17 +44,6 @@ mv $prefix/bin/curl $prefix/bin/.curl-wrapped
 mv $prefix/bin/wget $prefix/bin/.wget-wrapped
 mv $prefix/bin/scp $prefix/bin/.scp-wrapped
 
-mkdir -p $prefix/share/oh-my-zsh/custom/plugins/starship
-$prefix/bin/starship init zsh >$prefix/share/oh-my-zsh/custom/plugins/starship/starship.plugin.zsh
-$prefix/bin/starship completions zsh >$prefix/share/oh-my-zsh/custom/plugins/starship/_starship
-mkdir -p $prefix/share/oh-my-zsh/custom/plugins/atuin
-$prefix/bin/atuin init zsh --disable-up-arrow >$prefix/share/oh-my-zsh/custom/plugins/atuin/atuin.plugin.zsh
-$prefix/bin/atuin gen-completions --shell zsh >$prefix/share/oh-my-zsh/custom/plugins/atuin/_atuin
-mkdir -p $prefix/share/oh-my-zsh/custom/plugins/fzf
-cp $prefix/share/fzf/completion.zsh $prefix/share/oh-my-zsh/custom/plugins/fzf/_fzf_completion
-cp $prefix/share/fzf/key-bindings.zsh $prefix/share/oh-my-zsh/custom/plugins/fzf/key-bindings.zsh
-cp $prefix/share/oh-my-zsh/custom/plugins/conda-zsh-completion/_conda $prefix/share/zsh/site-functions
-
 find $prefix -name "*.a" -delete
 find $prefix -name "*.pyc" -delete
 rm -rf \
