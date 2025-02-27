3197a12c6f2e59e9ef7f3e49c2f214b0a2b50691
curoky <cccuroky@gmail.com>
2025-02-27 11:07:00 +0800

    2025-02-27 11:07:00+08:00

--- prebuilt/macos/pack-conda.sh
+++ prebuilt/macos/pack-conda.sh
@@ -11,10 +11,18 @@ rm -rf tmp
 mkdir -p tmp
 curl -sSL -o tmp/miniconda.sh \
   https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh
-
+# https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sed -i -e 's/| md5/| openssl md5/g' tmp/miniconda.sh
 bash tmp/miniconda.sh -b -u -p /opt/conda
 
+# setup env
 /workspace/dotbox/docker/stage/conda/setup-conda-env.sh -e ../../host/mac/conf/conda/py3.yaml
+
+# setup pipx
+/opt/conda/bin/pip3 install pipx
+export PIPX_HOME=/opt/conda/pipx
+export PIPX_BIN_DIR=/opt/conda/pipx/bin
+export PIPX_MAN_DIR=/opt/conda/pipx/share/man
+/opt/conda/bin/pipx install licenseheaders
 
 tar -c --gunzip -f tmp/conda.darwin-arm64.tar.gz /opt/conda
