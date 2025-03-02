1caa4bc0b0e79ac38369dd27be4d2189f037f0dc
curoky <cccuroky@gmail.com>
2025-03-03 00:06:31 +0800

    2025-03-03 00:06:31+08:00

--- prebuilt/macos/install-conda.sh
+++ prebuilt/macos/install-conda.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
+curl -L -o /tmp/conda.darwin-arm64.tar.gz \
+  https://github.com/curoky/dotbox/releases/download/v1.0/conda.darwin-arm64.tar.gz
+
 sudo rm -rf /opt/conda
 sudo mkdir /opt/conda
 sudo chown "$(id -u):$(id -g)" /opt/conda
 
-curl -L -o /tmp/conda.darwin-arm64.tar.gz \
-  https://github.com/curoky/dotbox/releases/download/v1.0/conda.darwin-arm64.tar.gz
 tar -x --gunzip -f /tmp/conda.darwin-arm64.tar.gz -C /opt/conda --strip-components=2
 rm -rf /tmp/conda.darwin-arm64.tar.gz
+12
