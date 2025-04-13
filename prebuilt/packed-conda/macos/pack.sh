0fdbb308b270132d60f50829aa08bd6b03bfff24
curoky <cccuroky@gmail.com>
2025-04-14 04:02:05 +0800

    2025-04-14 04:02:05+08:00

--- prebuilt/packed-conda/macos/pack.sh
+++ prebuilt/packed-conda/macos/pack.sh
@@ -16,8 +16,8 @@ sed -i -e 's/| md5/| openssl md5/g' tmp/miniconda.sh
 bash tmp/miniconda.sh -b -u -p /opt/conda
 
 # setup env
-../../docker/stage/conda/setup-conda-env.sh -e ../../host/mac/conf/conda/py3.yaml --conda_root=/opt/conda
-../../docker/stage/conda/setup-conda-env.sh -e ../../host/mac/conf/conda/monopoly.yaml --conda_root=/opt/conda
+../../../docker/stage/conda/setup-conda-env.sh -e ../../host/mac/conf/conda/py3.yaml --conda_root=/opt/conda
+../../../docker/stage/conda/setup-conda-env.sh -e ../../host/mac/conf/conda/monopoly.yaml --conda_root=/opt/conda
 
 # setup pipx
 /opt/conda/bin/pip3 install pipx
