2bf8c8b9c93f32dfb898880ec5068e839bd8e54a
curoky <cccuroky@gmail.com>
2025-04-14 21:34:47 +0800

    2025-04-14 21:34:47+08:00

--- prebuilt/prebuilt-sre-tools/common/installer.sh
+++ prebuilt/prebuilt-sre-tools/common/installer.sh
@@ -2,8 +2,41 @@
 set -xeuo pipefail
 
 abspath=$(cd "$(dirname "$0")" && pwd)
-target=${1:-/tmp/prebuilt}
+
+target=$HOME/app/prebuilt
+post_link=0
+
+while getopts "t:p:" opt; do
+  case "$opt" in
+    t)
+      target="$OPTARG"
+      ;;
+    p)
+      post_link=1
+      ;;
+    \?)
+      echo "Usage: $0 [-t target] [-p post link]"
+      exit 1
+      ;;
+  esac
+done
 
 rm -rf $target
 mkdir -p $target
 cp -r $abspath/* $target
+
+if [ ! -z "$post_link" ]; then
+  ln -s $target $HOME/prebuilt
+
+  if ! grep -q 'prebuilt/bin' ~/.bashrc; then
+    echo 'export PATH=$HOME/app/prebuilt/bin:$PATH' >>~/.bashrc
+  fi
+  if ! grep -q 'prebuilt/bin' ~/.profile; then
+    echo 'export PATH=$HOME/app/prebuilt/bin:$PATH' >>~/.profile
+  fi
+
+  rm -f ~/dotbox
+  ln -s $HOME/app/prebuilt/dotbox ~/dotbox
+
+  $HOME/app/prebuilt/dotbox/config/setup.sh host-linux $HOME/app/prebuilt/dotbox/config
+fi
