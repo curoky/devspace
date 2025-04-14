3c5564e5edfb0dda0871ed79c38a73cc1a16bcb2
curoky <cccuroky@gmail.com>
2025-04-15 02:51:25 +0800

    2025-04-15 02:51:25+08:00

--- prebuilt/packed-conda/darwin/pack.sh
+++ prebuilt/packed-conda/darwin/pack.sh
@@ -3,29 +3,29 @@ set -xeuo pipefail
 
 export PATH=$PATH:/sbin
 
-sudo rm -rf /opt/conda
-sudo mkdir -p /opt/conda
-sudo chown "$(id -u):$(id -g)" /opt/conda
+if ! id -u x &>/dev/null; then
+  sudo mkdir -p /Users/x/
+  sudo chown -R $(id -u):$(id -g) /Users/x/
+fi
 
-rm -rf tmp
-mkdir -p tmp
+rm -rf tmp && mkdir -p tmp
 curl -sSL -o tmp/miniconda.sh \
   https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh
 sed -i -e 's/| md5/| openssl md5/g' tmp/miniconda.sh
-bash tmp/miniconda.sh -b -u -p /opt/conda
+bash tmp/miniconda.sh -b -u -p /Users/x/app/conda
 
 # setup env
-../../../docker/stage/conda/setup-conda-env.sh -e ../../../host/darwin/conf/conda/py3.yaml --conda_root=/opt/conda
-../../../docker/stage/conda/setup-conda-env.sh -e ../../../host/darwin/conf/conda/monopoly.yaml --conda_root=/opt/conda
+../../../docker/stage/conda/setup-conda-env.sh -e ../../../host/darwin/conf/conda/py3.yaml --conda_root=/Users/x/app/conda
+../../../docker/stage/conda/setup-conda-env.sh -e ../../../host/darwin/conf/conda/monopoly.yaml --conda_root=/Users/x/app/conda
 
 # setup pipx
-/opt/conda/bin/pip3 install pipx
-export PIPX_HOME=/opt/conda/pipx
+/Users/x/app/conda/bin/pip3 install pipx
+export PIPX_HOME=/Users/x/app/conda/pipx
 export PIPX_BIN_DIR=${PIPX_HOME}/bin
 export PIPX_MAN_DIR=${PIPX_HOME}/share/man
-/opt/conda/bin/pipx install licenseheaders
+/Users/x/app/conda/bin/pipx install licenseheaders
 
-cp installer.sh /opt/conda/installer.sh
+cp installer.sh /Users/x/app/conda/installer.sh
 
-makeself --complevel 6 --tar-quietly --gzip --threads 16 /opt/conda tmp/conda.darwin-arm64.gzip.sh "Conda Installer" ./installer.sh
-makeself --complevel 16 --tar-quietly --zstd --threads 16 /opt/conda tmp/conda.darwin-arm64.zstd.sh "Conda Installer" ./installer.sh
+makeself --complevel 6 --tar-quietly --gzip --threads 16 /Users/x/app/conda tmp/conda.darwin-arm64.gzip.sh "Conda Installer" ./installer.sh
+makeself --complevel 16 --tar-quietly --zstd --threads 16 /Users/x/app/conda tmp/conda.darwin-arm64.zstd.sh "Conda Installer" ./installer.sh
