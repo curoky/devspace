400edbf9bb9190a9f0765a95b15deb4e0c16e23b
curoky <cccuroky@gmail.com>
2025-04-14 14:32:34 +0800

    2025-04-14 14:32:34+08:00

--- prebuilt/docker-offline-installer/debian10/install.sh
+++ prebuilt/docker-offline-installer/debian10/install.sh
@@ -19,29 +19,63 @@ set -xeuo pipefail
 
 enable_gpu=${1:-0}
 
-apt-get remove -y \
-  docker-ce docker-ce-cli containerd.io \
-  docker-buildx-plugin docker-compose-plugin \
-  nvidia-container-toolkit nvidia-container-toolkit-base \
-  libnvidia-container-tools libnvidia-container1 || echo ignore
+packages=(docker-ce docker-ce-cli containerd.io
+  docker-buildx-plugin docker-compose-plugin
+  nvidia-container-toolkit nvidia-container-toolkit-base
+  libnvidia-container-tools libnvidia-container1
+)
+
+for package in "${packages[@]}"; do
+  apt-get remove -y "$package" || true
+done
 
 apt-get update -y
-apt-get install -y iptables
+# apt-get install -y iptables
 
-dpkg -i tmp/docker/libseccomp2_2.5.4-1+b3_amd64.deb
+# dpkg: warning: downgrading containerd.io from 1.6.33-1 to 1.6.22-1
+# dpkg: warning: downgrading docker-ce from 5:26.1.4-1~debian.10~buster to 5:24.0.5-1~debian.11~bullseye
+# dpkg: warning: downgrading docker-ce-cli from 5:26.1.4-1~debian.10~buster to 5:24.0.5-1~debian.11~bullseye
+# dpkg: warning: downgrading docker-compose-plugin from 2.27.1-1~debian.10~buster to 2.20.2-1~debian.11~bullseye
 
-dpkg -i tmp/docker/containerd.io_1.6.22-1.deb \
-  tmp/docker/docker-ce_24.0.5-1~debian.11~bullseye.deb \
-  tmp/docker/docker-ce-cli_24.0.5-1~debian.11~bullseye.deb \
-  tmp/docker/docker-buildx-plugin_0.16.2-1~debian.11~bullseye.deb \
-  tmp/docker/docker-compose-plugin_2.20.2-1~debian.11~bullseye.deb
+urls=(
+  https://ftp.debian.org/debian/pool/main/libs/libseccomp/libseccomp2_2.6.0-2_amd64.deb
 
-if [[ $enable_gpu == 1 ]]; then
-  dpkg -i tmp/docker/libnvidia-container1_1.13.5-1.deb \
-    tmp/docker/libnvidia-container-tools_1.13.5-1.deb \
-    tmp/docker/nvidia-container-toolkit-base_1.13.5-1.deb \
-    tmp/docker/nvidia-container-toolkit_1.13.5-1.deb
-fi
+  # https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/
+  # https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/containerd.io_1.6.22-1_amd64.deb
+  # https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/docker-ce-cli_24.0.5-1~debian.11~bullseye_amd64.deb
+  # https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/docker-ce_24.0.5-1~debian.11~bullseye_amd64.deb
+  # https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/docker-buildx-plugin_0.16.2-1~debian.11~bullseye_amd64.deb
+  # https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/docker-compose-plugin_2.20.2-1~debian.11~bullseye_amd64.deb
+
+  https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/containerd.io_1.7.27-1_amd64.deb
+  https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/docker-ce-cli_28.0.4-1~debian.11~bullseye_amd64.deb
+  https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/docker-ce_28.0.4-1~debian.11~bullseye_amd64.deb
+  https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/docker-buildx-plugin_0.22.0-1~debian.11~bullseye_amd64.deb
+  https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/docker-compose-plugin_2.29.7-1~debian.11~bullseye_amd64.deb
+
+  # https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
+  # https://github.com/NVIDIA/libnvidia-container/raw/gh-pages/stable/debian10/amd64/libnvidia-container1_1.13.5-1_amd64.deb
+  # https://github.com/NVIDIA/libnvidia-container/raw/gh-pages/stable/debian10/amd64/libnvidia-container-tools_1.13.5-1_amd64.deb
+  # https://github.com/NVIDIA/libnvidia-container/raw/gh-pages/stable/debian10/amd64/nvidia-container-toolkit-base_1.13.5-1_amd64.deb
+  # https://github.com/NVIDIA/libnvidia-container/raw/gh-pages/stable/debian10/amd64/nvidia-container-toolkit_1.13.5-1_amd64.deb
+
+  https://github.com/NVIDIA/libnvidia-container/raw/gh-pages/stable/deb/amd64/libnvidia-container1_1.17.5-1_amd64.deb
+  https://github.com/NVIDIA/libnvidia-container/raw/gh-pages/stable/deb/amd64/libnvidia-container-tools_1.17.5-1_amd64.deb
+  https://github.com/NVIDIA/libnvidia-container/raw/gh-pages/stable/deb/amd64/nvidia-container-toolkit-base_1.17.5-1_amd64.deb
+  https://github.com/NVIDIA/libnvidia-container/raw/gh-pages/stable/deb/amd64/nvidia-container-toolkit_1.17.5-1_amd64.deb
+)
+
+dpkg_command="dpkg -i"
+
+for url in "${urls[@]}"; do
+  filename=$(basename "$url")
+  dpkg_command="$dpkg_command tmp/docker/$filename"
+done
+
+eval "$dpkg_command"
+
+systemctl daemon-reload
+systemctl restart docker
 
 # Post stage for docker
 
@@ -59,6 +93,3 @@ fi
 # }
 
 # docker run --rm --runtime=nvidia --gpus all ubuntu nvidia-smi
-
-systemctl daemon-reload
-systemctl restart docker
--- prebuilt/docker-offline-installer/debian10/pack.sh
+++ prebuilt/docker-offline-installer/debian10/pack.sh
@@ -1,38 +1,38 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
+urls=(
+  https://ftp.debian.org/debian/pool/main/libs/libseccomp/libseccomp2_2.6.0-2_amd64.deb
+
+  # https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/containerd.io_1.6.22-1_amd64.deb
+  # https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/docker-ce-cli_24.0.5-1~debian.11~bullseye_amd64.deb
+  # https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/docker-ce_24.0.5-1~debian.11~bullseye_amd64.deb
+  # https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/docker-buildx-plugin_0.16.2-1~debian.11~bullseye_amd64.deb
+  # https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/docker-compose-plugin_2.20.2-1~debian.11~bullseye_amd64.deb
+
+  https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/containerd.io_1.7.27-1_amd64.deb
+  https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/docker-ce-cli_28.0.4-1~debian.11~bullseye_amd64.deb
+  https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/docker-ce_28.0.4-1~debian.11~bullseye_amd64.deb
+  https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/docker-buildx-plugin_0.22.0-1~debian.11~bullseye_amd64.deb
+  https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/docker-compose-plugin_2.29.7-1~debian.11~bullseye_amd64.deb
+
+  # https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
+  # https://github.com/NVIDIA/libnvidia-container/raw/gh-pages/stable/debian10/amd64/libnvidia-container1_1.13.5-1_amd64.deb
+  # https://github.com/NVIDIA/libnvidia-container/raw/gh-pages/stable/debian10/amd64/libnvidia-container-tools_1.13.5-1_amd64.deb
+  # https://github.com/NVIDIA/libnvidia-container/raw/gh-pages/stable/debian10/amd64/nvidia-container-toolkit-base_1.13.5-1_amd64.deb
+  # https://github.com/NVIDIA/libnvidia-container/raw/gh-pages/stable/debian10/amd64/nvidia-container-toolkit_1.13.5-1_amd64.deb
+
+  https://github.com/NVIDIA/libnvidia-container/raw/gh-pages/stable/deb/amd64/libnvidia-container1_1.17.5-1_amd64.deb
+  https://github.com/NVIDIA/libnvidia-container/raw/gh-pages/stable/deb/amd64/libnvidia-container-tools_1.17.5-1_amd64.deb
+  https://github.com/NVIDIA/libnvidia-container/raw/gh-pages/stable/deb/amd64/nvidia-container-toolkit-base_1.17.5-1_amd64.deb
+  https://github.com/NVIDIA/libnvidia-container/raw/gh-pages/stable/deb/amd64/nvidia-container-toolkit_1.17.5-1_amd64.deb
+)
+
 rm -rf tmp/docker
 mkdir -p tmp/docker
 
-curl -sSL -o tmp/docker/libseccomp2_2.5.4-1+b3_amd64.deb \
-  http://ftp.debian.org/debian/pool/main/libs/libseccomp/libseccomp2_2.5.4-1+b3_amd64.deb
-
-# https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/
-
-pkg_list=(
-  containerd.io_1.6.22-1
-  docker-ce-cli_24.0.5-1~debian.11~bullseye
-  docker-ce_24.0.5-1~debian.11~bullseye
-  # docker-buildx-plugin_0.11.2-1~debian.11~bullseye
-  docker-buildx-plugin_0.16.2-1~debian.11~bullseye
-  docker-compose-plugin_2.20.2-1~debian.11~bullseye
-)
-
-for pkg in ${pkg_list[@]}; do
-  curl -sSL -o tmp/docker/${pkg}.deb \
-    https://download.docker.com/linux/debian/dists/bullseye/pool/stable/amd64/${pkg}_amd64.deb
-done
-
-# https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
-pkg_list=(
-  libnvidia-container1_1.13.5-1
-  libnvidia-container-tools_1.13.5-1
-  nvidia-container-toolkit-base_1.13.5-1
-  nvidia-container-toolkit_1.13.5-1
-)
-for pkg in ${pkg_list[@]}; do
-  curl -sSL -o tmp/docker/${pkg}.deb \
-    https://github.com/NVIDIA/libnvidia-container/raw/gh-pages/stable/debian10/amd64/${pkg}_amd64.deb
+for url in "${urls[@]}"; do
+  curl -sSL -o tmp/docker/$(basename "$url") "$url"
 done
 
 cd tmp
