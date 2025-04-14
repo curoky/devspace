a086b6135bbfbdb82d76979c2724e970a30f2011
curoky <cccuroky@gmail.com>
2025-04-14 14:52:17 +0800

    2025-04-14 14:52:17+08:00

---
+++ prebuilt/packed-conda/linux/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+curl -L -o /tmp/conda.linux-x86_64.tar.gz \
+  https://github.com/curoky/dotbox/releases/download/v1.0/conda.linux-x86_64.tar.gz
+
+sudo rm -rf /opt/conda
+sudo mkdir /opt/conda
+sudo chown "$(id -u):$(id -g)" /opt/conda
+
+tar -x --gunzip -f /tmp/conda.linux-x86_64.tar.gz -C /opt/conda --strip-components=2
+rm -rf /tmp/conda.linux-x86_64.tar.gz
