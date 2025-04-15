9294f05720c39209c6339cafe9cad9c07cc631c2
curoky <cccuroky@gmail.com>
2025-04-15 18:08:51 +0800

    2025-04-15 18:08:51+08:00

--- tools/profile-installer.sh
+++ tools/profile-installer.sh
@@ -2,7 +2,8 @@
 set -xeuo pipefail
 
 curl -L -o /tmp/profile.sh https://github.com/curoky/dotbox/raw/dev/config/passkey/profile.gzip.sh
+
 bash /tmp/profile.sh $@
 
 # Usage
-# curl -sSL https://github.com/curoky/dotbox/raw/dev/tools/profile-installer.sh | bash
+# curl -sSL https://github.com/curoky/dotbox/raw/dev/tools/profile-installer.sh | bash -s -- --ssl-pass-src pass:xxx
