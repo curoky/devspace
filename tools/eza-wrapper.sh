b563f50f8b9a0831ede8fc2f628719d2c7086454
curoky <cccuroky@gmail.com>
2025-04-17 11:22:45 +0800

    2025-04-17 11:22:45+08:00

--- tools/eza-wrapper.sh
+++ tools/eza-wrapper.sh
@@ -1,21 +1,9 @@
 #!/usr/bin/env bash
-# Copyright (c) 2018-2024 curoky(cccuroky@gmail.com).
-#
-# This file is part of dotbox.
-# See https://github.com/curoky/dotbox for further info.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# https://gist.github.com/eggbean/74db77c4f6404dd1f975bd6f048b86f8
 
+if ! command -v exa &>/dev/null; then
+  exec /bin/ls "$@"
+fi
 # If stdout isn't terminal, fallback to default ls.
 if [[ ! -t 1 ]]; then
   exec /bin/ls "$@"
