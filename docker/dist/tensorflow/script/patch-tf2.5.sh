255ca400e208c5d6778feb8c9442ea7e84caa50a
curoky <cccuroky@gmail.com>
2025-04-23 22:28:46 +0800

    2025-04-23 22:28:46+08:00

--- docker/dist/tensorflow/script/patch-tf2.5.sh
+++ docker/dist/tensorflow/script/patch-tf2.5.sh
@@ -19,5 +19,7 @@
 set -xeuo pipefail
 
 sed -i -e "s|third_party/gpus/cuda/include/||g" \
-  /home/x/app/conda/envs/tf2.5/lib/python3.7/site-packages/tensorflow/include/tensorflow/core/util/gpu_kernel_helper.h \
-  /home/x/app/conda/envs/tf2.5/lib/python3.7/site-packages/tensorflow/include/tensorflow/core/util/gpu_device_functions.h
+  /home/x/app/conda/envs/tf2.5-abi0/lib/python3.7/site-packages/tensorflow/include/tensorflow/core/util/gpu_kernel_helper.h \
+  /home/x/app/conda/envs/tf2.5-abi0/lib/python3.7/site-packages/tensorflow/include/tensorflow/core/util/gpu_device_functions.h \
+  /home/x/app/conda/envs/tf2.5-abi1/lib/python3.7/site-packages/tensorflow/include/tensorflow/core/util/gpu_kernel_helper.h \
+  /home/x/app/conda/envs/tf2.5-abi1/lib/python3.7/site-packages/tensorflow/include/tensorflow/core/util/gpu_device_functions.h
