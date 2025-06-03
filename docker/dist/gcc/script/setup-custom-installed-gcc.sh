5ef5981c0b970c1ed6f9c12e6bf94107011806f5
curoky <cccuroky@gmail.com>
2025-06-04 01:30:46 +0800

    2025-06-04 01:30:46+08:00

--- docker/dist/gcc/script/setup-custom-installed-gcc.sh
+++ docker/dist/gcc/script/setup-custom-installed-gcc.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-find /home/x/app/gcc/gcc-14.2.0/bin/ -type f -exec ln -sf {} /usr/bin \;
+find /opt/gcc/gcc-14.2.0/bin/ -type f -exec ln -sf {} /usr/bin \;
 find /home/x/app/binutils/bin/ -type f -exec ln -sf {} /usr/bin \;
 
-echo '/home/x/app/gcc/gcc-14.2.0/lib64' >>/etc/ld.so.conf.d/000-gcc14.conf
+echo '/opt/gcc/gcc-14.2.0/lib64' >>/etc/ld.so.conf.d/000-gcc14.conf
 ldconfig -v
