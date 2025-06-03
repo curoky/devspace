728cdae1bd5261aaaa6f3031a70284a336bdce2a
curoky <cccuroky@gmail.com>
2025-06-03 13:52:07 +0800

    2025-06-03 13:52:07+08:00

--- docker/stage/gcc/build.sh
+++ docker/stage/gcc/build.sh
@@ -4,5 +4,5 @@ set -xeuo pipefail
 docker buildx build . \
   --file Dockerfile \
   --network=host \
-  --build-arg="GCC_VERSION=14.2.0"
---tag curoky/dotbox:stage-gcc-14.2.0
+  --build-arg="GCC_VERSION=14.2.0" \
+  --tag curoky/dotbox:stage-gcc-14.2.0
