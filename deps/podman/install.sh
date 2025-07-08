b57049713cad53b7610386c8b7637d3e8dc04cdd
curoky <cccuroky@gmail.com>
2025-07-08 23:32:04 +0800

    2025-07-08 23:32:04+08:00

--- deps/podman/install.sh
+++ deps/podman/install.sh
@@ -5,12 +5,11 @@ set -xeuo pipefail
 # tar -x -f podman.tar
 
 rm -rf /opt/mypodman
-mkdir -p /opt/mypodman /opt/mypodman/testdir
+mkdir -p /opt/mypodman
 cp -r ./* /opt/mypodman
 
-rm -rf /etc/containers
-mkdir -p /etc/containers
-cp -r ./conf/* /etc/containers
+rm -rf /etc/containers/policy.json
+cp ./conf/policy.json /etc/containers/policy.json
 
 rm -rf /etc/systemd/system/mypodmand.service
 mkdir -p /etc/systemd/system/
