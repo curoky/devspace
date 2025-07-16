e60a28326e9afe4108d99a6b29a159de26e9ac4c
curoky <cccuroky@gmail.com>
2025-07-16 11:02:34 +0800

    2025-07-16 11:02:34+08:00

--- dist/image/gcc/script/setup-custom-installed-gcc.sh
+++ dist/image/gcc/script/setup-custom-installed-gcc.sh
@@ -17,8 +17,8 @@
 # limitations under the License.
 set -xeuo pipefail
 
-find /opt/gcc/gcc-14.2.0/bin/ -type f -exec ln -sf {} /usr/bin \;
+find /opt/gcc/gcc-15.1.0/bin/ -type f -exec ln -sf {} /usr/bin \;
 find /opt/binutils/bin/ -type f -exec ln -sf {} /usr/bin \;
 
-echo '/opt/gcc/gcc-14.2.0/lib64' >>/etc/ld.so.conf.d/000-gcc14.conf
+echo '/opt/gcc/gcc-15.1.0/lib64' >>/etc/ld.so.conf.d/000-gcc15.conf
 ldconfig -v
