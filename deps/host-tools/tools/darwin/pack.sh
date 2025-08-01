bd4fb640006e4bb56a9b36b4011e3ff52b7208f0
curoky <cccuroky@gmail.com>
2025-08-01 22:25:40 +0800

    2025-08-01 22:25:40+08:00

--- deps/host-tools/tools/darwin/pack.sh
+++ deps/host-tools/tools/darwin/pack.sh
@@ -125,10 +125,20 @@ unzip -d tmp/tools/Library tmp/tools/downloads/Kap.zip
 unzip -d tmp/tools/Library tmp/tools/downloads/KeepingYouAwake.zip
 unzip -d tmp/tools/Library tmp/tools/downloads/istatmenus6.zip
 
-cp tmp/tools/downloads/OBS-Studio.dmg tmp/tools/Library
-cp tmp/tools/downloads/Snipaste.dmg tmp/tools/Library
-cp tmp/tools/downloads/IINA.dmg tmp/tools/Library
-cp tmp/tools/downloads/Keka.dmg tmp/tools/Library
+hdiutil attach tmp/tools/downloads/OBS-Studio.dmg
+hdiutil attach tmp/tools/downloads/Snipaste.dmg
+hdiutil attach tmp/tools/downloads/IINA.dmg
+hdiutil attach tmp/tools/downloads/keka.dmg
+
+cp -r '/Volumes/OBS Studio 31.1.2 (Apple)/OBS.app' tmp/tools/Library
+cp -r '/Volumes/Snipaste/Snipaste.app' tmp/tools/Library
+cp -r '/Volumes/IINA/IINA.app' tmp/tools/Library
+cp -r '/Volumes/Keka/Keka.app' tmp/tools/Library
+
+hdiutil detach '/Volumes/Snipaste'
+hdiutil detach '/Volumes/IINA'
+hdiutil detach '/Volumes/Keka'
+hdiutil detach '/Volumes/OBS Studio 31.1.2 (Apple)'
 
 rm -rf tmp/tools/downloads
 cp -f ../common/installer.sh tmp/tools/
