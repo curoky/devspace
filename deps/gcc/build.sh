2ce403942dc63efec4316c762407d84ae16f2733
curoky <cccuroky@gmail.com>
2025-09-04 18:21:11 +0800

    2025-09-04 18:21:11+08:00

--- deps/gcc/build.sh
+++ deps/gcc/build.sh
@@ -17,8 +17,10 @@
 # limitations under the License.
 set -xeuo pipefail
 
+version=${1:-14.2.0}
 docker build . \
   --file Dockerfile \
   --network=host \
-  --build-arg="GCC_VERSION=14.2.0" \
-  --tag curoky/devspace:deps-gcc-14.2.0
+  --build-arg="GCC_VERSION=$version" \
+  --build-arg="ENABLE_2STAGE=false" \
+  --tag curoky/devspace:deps-gcc-$version
