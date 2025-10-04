93bb94dfa5d32d0b0b9ab31a9db27e26e3708e64
curoky <cccuroky@gmail.com>
2025-10-04 21:10:52 +0800

    2025-10-04 21:10:52+08:00

--- deps/uv-conda/uv-from-pip.sh
+++ deps/uv-conda/uv-from-pip.sh
@@ -2,6 +2,6 @@
 set -xeuo pipefail
 
 rm -rf .venv pyproject.toml uv.lock
-uv init --no-workspace 
+uv init --no-workspace
 uv add -r /workspace/devspace/dist/image/stage/conda/env/monopoly.yaml
 uv sync
