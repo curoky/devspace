8887859f210556b97ef191658d0340b25cdc94a1
curoky <cccuroky@gmail.com>
2026-04-26 03:22:49 +0800

    2026-04-26 03:22:49+08:00

--- dotfiles/zsh/lib/002-proxy.sh
+++ dotfiles/zsh/lib/002-proxy.sh
@@ -6,6 +6,6 @@ if [[ -f /var/run/s6/container_environment/HTTP_PROXY ]]; then
   export HTTPS_PROXY=$http_proxy
   export all_proxy=$http_proxy
   export ALL_PROXY=$http_proxy
-  export no_proxy=localhost,127.0.0.1,192.168.0.0/16,10.0.0.0/8
-  export NO_PROXY=localhost,127.0.0.1,192.168.0.0/16,10.0.0.0/8
+  export no_proxy=localhost,127.0.0.1,192.168.0.0/16,10.0.0.0/8,releases.nixos.org
+  export NO_PROXY=localhost,127.0.0.1,192.168.0.0/16,10.0.0.0/8,releases.nixos.org
 fi
