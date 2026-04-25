b8a6b042c5a9234e746156805c0ee1e41f0b0532
curoky <cccuroky@gmail.com>
2026-04-26 01:08:59 +0800

    2026-04-26 01:08:59+08:00

---
+++ dotfiles/zsh/lib/002-proxy.sh
@@ -0,0 +1,11 @@
+
+if [[ -f /var/run/s6/container_environment/HTTP_PROXY ]]; then
+  export http_proxy=$(cat /var/run/s6/container_environment/HTTP_PROXY)
+  export HTTP_PROXY=$http_proxy
+  export https_proxy=$http_proxy
+  export HTTPS_PROXY=$http_proxy
+  export all_proxy=$http_proxy
+  export ALL_PROXY=$http_proxy
+  export no_proxy=localhost,127.0.0.1
+  export NO_PROXY=localhost,127.0.0.1
+fi
