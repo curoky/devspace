69fc366c57f91501125df23c33f462a1ea5e2276
curoky <cccuroky@gmail.com>
2026-04-26 01:16:02 +0800

    2026-04-26 01:16:02+08:00

--- images/base/script/s6-init/oncetask.sh
+++ images/base/script/s6-init/oncetask.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-s6-setuidgid x env HOME=/home/x /opt/devspace/images/base/script/s6-init/entrypoint.sh
+s6-setuidgid x env HOME=/home/x /opt/devspace/images/base/script/s6-init/entrypoint.sh > /var/log/oncetask.log
