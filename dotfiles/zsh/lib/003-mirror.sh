040adbee5ab549f6cbf448585cf1afeaaaaa1922
curoky <cccuroky@gmail.com>
2026-05-02 17:10:21 +0800

    2026-05-02 17:10:21+08:00

---
+++ dotfiles/zsh/lib/003-mirror.sh
@@ -0,0 +1,5 @@
+function set-mirror() {
+  export UV_DEFAULT_INDEX=http://mirrors.aliyun.com/pypi/simple/
+  export PIP_INDEX_URL=http://mirrors.aliyun.com/pypi/simple/
+  # export PIP_EXTRA_INDEX_URL=http://mirrors.aliyun.com/pypi/simple/
+}
