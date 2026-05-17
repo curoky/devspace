f00f6e8a92613c5b091507142cedbe1df3c3718c
curoky <cccuroky@gmail.com>
2026-05-17 15:35:41 +0800

    2026-05-17 15:35:41+08:00

--- images/base/script/setup-user.sh
+++ images/base/script/setup-user.sh
@@ -21,10 +21,10 @@ set -xeuo pipefail
 userdel ubuntu -r || echo "ignore userdel failed"
 
 # update user root
-echo "root:123456" | chpasswd
+echo "root:x123456" | chpasswd
 
 # add user x
 useradd --create-home --uid 5230 --user-group x
-echo "x:123456" | chpasswd
+echo "x:x123456" | chpasswd
 usermod -aG sudo x
 echo "x ALL=(ALL:ALL) NOPASSWD:ALL" >>/etc/sudoers.d/nopasswd_user
