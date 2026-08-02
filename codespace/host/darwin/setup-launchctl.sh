cd3215600f7ae4d7c22bf63527d167c7977ae770
curoky <cccuroky@gmail.com>
2026-08-03 03:53:02 +0800

    refactor: clean up Brewfile and update YAML schema detection settings

--- codespace/host/darwin/setup-launchctl.sh
+++ codespace/host/darwin/setup-launchctl.sh
@@ -5,6 +5,6 @@ launchctl bootout gui/"$(id -u)"/sh.atuin.daemon || true
 launchctl bootstrap gui/"$(id -u)" ~/Library/LaunchAgents/sh.atuin.daemon.plist
 launchctl kickstart -k gui/"$(id -u)"/sh.atuin.daemon
 
-launchctl bootout gui/"$(id -u)"/sh.atuin.server || true
-launchctl bootstrap gui/"$(id -u)" ~/Library/LaunchAgents/sh.atuin.server.plist
-launchctl kickstart -k gui/"$(id -u)"/sh.atuin.server
+# launchctl bootout gui/"$(id -u)"/sh.atuin.server || true
+# launchctl bootstrap gui/"$(id -u)" ~/Library/LaunchAgents/sh.atuin.server.plist
+# launchctl kickstart -k gui/"$(id -u)"/sh.atuin.server
