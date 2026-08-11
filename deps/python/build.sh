b40908ec47bd318e7ad1c7deb22c2dac1501855e
curoky <cccuroky@gmail.com>
2026-08-11 15:13:02 +0800

    chore: remove all deprecated docker build scripts and configs
    Clean up the entire deps directory containing old GCC, Python, LLVM, Tabby, and host-tools docker build workflows, build scripts, Taskfiles, and related assets that are no longer maintained or used.

--- deps/python/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -xeuo pipefail
-
-version=${1:-3.13.7}
-docker build . \
-  --file Dockerfile \
-  --network=host \
-  --build-arg="PYTHON_VERSION=${version}" \
-  --tag curoky/devspace:deps-python-${version}
--- deps/tabby/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -xeuo pipefail
-cd "$(dirname $0)" || exit 1
-
-docker build . --network=host --file Dockerfile "${@:2}" \
-  --tag curoky/devspace:tabby
